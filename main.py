3c5085e4baafa29405f7bfbb70290c4726439656
lelek <heschl.leon@gmail.com>
2026-09-16 22:51:24 +0200

    experimented with scapy

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from scapy.all import *
-from scapy.layers.l2 import Ether
-from scapy.sendrecv import AsyncSniffer
+from scapy.layers import dhcp
 
-print(conf.ifaces) # lists interfaces
+
+default_gateway = conf.route.route("0.0.0.0")[2]
+print(default_gateway)
 print(conf.route)
+
+#print(conf.ifaces) # lists interfaces
+#print(conf.route)
 
 a = Ether()
 
+#p = sr1(
+#    IP(dst="example.com") / ICMP() / "XXXXXXXXXXX"
+#)
 
+
+#reply = sr1(IP(dst="8.8.8.8")/ICMP())
+#if reply:
+#    reply.show()
